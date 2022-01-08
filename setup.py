9492a5e60d6e0c8fd613917af08e93a08a1c9e14
BenTenmann <benji.tenmann@me.com>
2022-01-08 14:26:47 +0000

    feat: update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-import os
+import re
 from pathlib import Path
 
 from setuptools import setup, find_packages
 
+DIR = Path(__file__).parent
+
 
 def main():
-    pass
+    try:
+        changelog = (DIR / 'CHANGELOG.md').read_text()
+        __version__ = re.findall(r"\[([0-9.]+)]", changelog)
+
+    except (FileNotFoundError, ValueError):
+        __version__ = '0.1.0'
+
+    long_description = (DIR / 'README.md').read_text()
+    setup(
+        name='bio-datasets',
+        version=__version__,
+        long_description=long_description
+    )
 
 
 if __name__ == '__main__':
